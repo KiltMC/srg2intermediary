d9c9fbfbb3671bc9fbf5e22e78ff5f2a8f7faea0
Naz <bluspringindohq@gmail.com>
2023-07-05 23:01:11 +0800

    Improve duplicate method name remapping

--- src/main/kotlin/xyz/bluspring/srg2intermediary/Remapper.kt
+++ src/main/kotlin/xyz/bluspring/srg2intermediary/Remapper.kt
@@ -2,7 +2,11 @@ package xyz.bluspring.srg2intermediary
 
 import net.minecraftforge.srgutils.IMappingBuilder
 import net.minecraftforge.srgutils.IMappingFile
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.ClassNode
 import java.io.File
+import java.util.jar.JarFile
 
 class Remapper(downloader: MappingDownloader, private val version: String) {
     val officialMappings = IMappingFile.load(downloader.srgMappingsFile) // official -> srg
@@ -11,7 +15,46 @@ class Remapper(downloader: MappingDownloader, private val version: String) {
     val mojangMappings = IMappingFile.load(downloader.mojangMappingsFile).reverse() // official -> moj (result of being reversed)
     val intermediaryMappings = IMappingFile.load(downloader.intermediaryFile) // official -> intermediary
 
+    val classParents = mutableMapOf<String, MutableList<String>>()
+
+    init {
+        val startTime = System.currentTimeMillis()
+
+        val jar = JarFile(downloader.clientFile)
+        for (entry in jar.entries()) {
+            if (!entry.name.endsWith(".class"))
+                continue
+
+            val stream = jar.getInputStream(entry)
+            val classReader = ClassReader(stream)
+            val classNode = ClassNode(Opcodes.ASM9)
+
+            classReader.accept(classNode, 0)
+
+            classParents[classNode.name] = mutableListOf(
+                classNode.superName
+            ).apply {
+                this.addAll(classNode.interfaces)
+            }
+        }
+
+        println("Loaded all Minecraft classes and mapped them to their parents. (took ${System.currentTimeMillis() - startTime} ms)")
+    }
+
     private val alreadyRemapped = mutableMapOf<String, Pair<String, String?>>()
+
+    // Class names are official mapped.
+    private fun getParentTree(className: String): List<String> {
+        val parents = classParents[className] ?: return listOf("java/lang/Object")
+        val tree = mutableListOf<String>()
+
+        for (parent in parents) {
+            tree.add(parent)
+            tree.addAll(getParentTree(parent))
+        }
+
+        return tree
+    }
 
     fun remap() {
         val startTime = System.currentTimeMillis()
@@ -26,7 +69,7 @@ class Remapper(downloader: MappingDownloader, private val version: String) {
                 val srgMethod = srgClass.getMethod(iMethod.original, iMethod.descriptor) ?: return@method
                 val mojMethod = mojClass.getMethod(iMethod.original, iMethod.descriptor) ?: return@method
 
-                alreadyRemapped[srgMethod.mapped] = Pair(iMethod.mapped, mojMethod.mappedDescriptor)
+                alreadyRemapped["${srgClass.mapped}:${srgMethod.mapped}"] = Pair(iMethod.mapped, mojMethod.mappedDescriptor)
             }
         }
 
@@ -87,11 +130,16 @@ class Remapper(downloader: MappingDownloader, private val version: String) {
                 if (method.original.startsWith("f_"))
                     return@methodMapper
 
-                if (alreadyRemapped.contains(method.original)) {
-                    val remapped = alreadyRemapped[method.original]!!
-                    mappedClass.method(remapped.second, method.original, remapped.first)
+                val tree = getParentTree(srgClass.original)
+                for (parent in tree) {
+                    val parentName = srgMappings.getClass(parent)?.mapped ?: continue
 
-                    return@methodMapper
+                    if (alreadyRemapped.contains("$parentName:${method.original}")) {
+                        val remapped = alreadyRemapped["$parentName:${method.original}"]!!
+                        mappedClass.method(remapped.second, method.original, remapped.first)
+
+                        return@methodMapper
+                    }
                 }
 
                 val intermediaryMethod = intermediaryClass.getMethod(method.mapped, method.mappedDescriptor)
